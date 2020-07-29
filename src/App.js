3ba59cfe479f251ab13a0d87713421abcb1057e1
suhailphp <suhailphp@gmail.com>
2020-07-29 12:56:12 +0400

    Logging API Calls succes and Errors using axios.interceptors

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ import "./App.css";
 const ApiEndPoint = "https://jsonplaceholder.typicode.com/posts";
 
 //this function for take all unexpected error Gobally. then no need to repeat everywhere
-axios.interceptors.response.use(null, (error) => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-  if (!expectedError) {
-    alert("Unexpected error occured, please try again");
-    console.log("unexpectedError", error);
+axios.interceptors.response.use(
+  (success) => {
+    console.log("API Call success", success);
+    return Promise.resolve(success);
+  },
+  (error) => {
+    const expectedError =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+    if (!expectedError) {
+      alert("Unexpected error occured, please try again");
+      console.log("unexpectedError", error);
+    }
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
+);
 
 class App extends Component {
   state = {
