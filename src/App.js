f8ec21af287ee8bd76ac22679909613b889549f2
suhailphp <suhailphp@gmail.com>
2020-07-29 10:20:03 +0400

    Deleting Data , using axios.delete and jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -37,8 +37,10 @@ class App extends Component {
     this.setState({ posts });
   };
 
-  handleDelete = (post) => {
-    console.log("Delete", post);
+  handleDelete = async (post) => {
+    await axios.delete(ApiEndPoint + "/" + post.id, post);
+    let posts = this.state.posts.filter((p) => p.id !== post.id);
+    this.setState({ posts });
   };
 
   render() {
